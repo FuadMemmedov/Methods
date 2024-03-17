a60f9bae24352aa5809958202bab65d2ff48fbc9
FuadMemmedov <tu7m43u29@code.edu.az>
2024-03-17 18:14:58 +0400

    task1

---
+++ Methods1/Methods1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Methods1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "fuad", "arif", "adil", "elmir" };
+            string[] arr2 = { "zaman", "fuad", "vasif" };
+            SameNames(arr, arr2);
+
+        }
+        static void SameNames(string[] a, string[] b)
+        {
+            int c = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++)
+                {
+                    if (a[i] == b[j])
+                    {
+                        Console.Write(a[i]);
+                        c = 1;
+                        break;
+
+
+                    }
+
+
+                }
+            }
+
+
+            if (c == 0)
+            {
+
+                Console.WriteLine("Ortaq ad yoxdu");
+                
+
+            }
+        }
+    }    
+}
