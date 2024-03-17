89aca5f5d22ad53db90d398490648648da7d2c4f
FuadMemmedov <tu7m43u29@code.edu.az>
2024-03-17 18:15:51 +0400

    task3

---
+++ Method3/Method3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Method3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = 'A';
+            string[] arr = { "Fuad", "Adil", "Zaman", "Murad", "Azer"};
+
+            Names(arr, a);
+
+
+        }
+        static void Names(string[] arr, char a)
+        {
+            int c = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                    if(a == arr[i][0])
+                    {
+                        Console.WriteLine(arr[i]);
+                         c = 1;
+                        
+
+                    }
+                    
+            }
+            if (c == 0)
+            {
+                Console.WriteLine("Bu herfle baslayan soz yoxdur");
+            }
+        }
+    }
+}
